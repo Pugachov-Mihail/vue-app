4d87c94129fbf0349109c6cb8e5c27d299f94b91
xd383727@gmail.com <@!lodS9506>
2023-07-13 17:20:53 +0300

    create app

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <PostForm @create="createPost"/>
+    <PostList :posts="posts"/>
+  </div>
+</template>
+
+<script>
+import PostForm from "@/components/PostForm";
+import PostList from "@/components/PostList";
+
+export default {
+  name: "App",
+  components: {PostForm, PostList},
+  data(){
+    return {
+     posts: [
+       {id: 1, title: "Какое то сообщение", body: "Какой то текст"},
+       {id: 2, title: "Какое то сообщение 2", body: "Какой то текст 2"},
+     ],
+    }
+  },
+  methods: {
+    createPost(post){
+        this.posts.push(post)
+    },
+  }
+}
+</script>
+
+<style>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+</style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h3>Список пользователей</h3>
+  </div>
+  <post-item
+      :post="post"
+      v-for="post in posts" />
+</template>
+
+<script>
+import PostItem from "@/components/PostItem";
+export default {
+  name: "PostList",
+  components: {PostItem},
+  props: {
+    posts: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
